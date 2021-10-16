f79a10bfce008d69a47dc3c064de7996e7ebb631
Guilherme Passos <guilhermep@esparta.io>
2021-10-16 12:33:48 -0300

    finalizando primeiros testes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,73 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+
+/* Styles */
+import styles from '../styles/index.module.css';
+import triangleStyles from '../styles/triangle.module.css';
+
+interface ITriangle {
+  a: number;
+  b: number;
+  c: number;
+}
 
 const Home: NextPage = () => {
-  const a: number = 3;
-  const b: number = 4;
-  const c: number = 5;
+  const [triangle, setTriangle] = useState<ITriangle>({a: 30, b: 40, c: 50});
 
-  console.log((c * c) === (a*a) + (b*b))
+  const applyPythagoreanTheorem = () => {
+    return (
+      (triangle.a * triangle.a) === (triangle.b * triangle.b) + (triangle.c * triangle.c)
+      || (triangle.b * triangle.b) === (triangle.a * triangle.a) + (triangle.c * triangle.c)
+      || (triangle.c * triangle.c) === (triangle.a * triangle.a) + (triangle.b * triangle.b)
+        ? 'Triângulo Retângulo'
+        : 'Não é um triângulo retângulo'
+    )
+  }
 
   return (
-    <div>
-      <p>{(c * c) === (a*a) + (b*b) ? 'True' : 'False'}</p>
+    <div className={styles.pageWrapper}>
+      <div className={styles.container}>
+        <input type="number" min={1} onChange={e => setTriangle({...triangle, a: parseFloat(e.target.value)})} value={triangle.a}/>
+        <input type="number" min={1} onChange={e => setTriangle({...triangle, b: parseFloat(e.target.value)})} value={triangle.b}/>
+        <input type="number" min={1} onChange={e => setTriangle({...triangle, c: parseFloat(e.target.value)})} value={triangle.c}/>
+
+        <div>
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={triangle.a}
+            onChange={e => setTriangle({...triangle, a: parseInt(e.target.value)})}
+          />
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={triangle.b}
+            onChange={e => setTriangle({...triangle, b: parseInt(e.target.value)})}
+          />
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={triangle.c}
+            onChange={e => setTriangle({...triangle, c: parseInt(e.target.value)})}
+          />
+        </div>
+
+        <div className={triangleStyles.triangleContainer}>
+          <div 
+            className={triangleStyles.triangle}
+            style={{
+              borderTopWidth: `${triangle.a}px`,
+              borderBottomWidth: `${triangle.b}px`,
+              borderLeftWidth: `${triangle.c}px`,
+            }}
+          />
+        </div>
+
+        <h1>{applyPythagoreanTheorem()}</h1>
+      </div>
     </div>
   )
 }
