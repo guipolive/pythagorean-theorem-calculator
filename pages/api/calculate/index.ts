2088da23df82018cf8e59be9ba6407be6b3d2695
Guilherme Passos <guilhermep@esparta.io>
2021-10-17 17:05:48 -0300

    removendo arquivo desnecessário

--- pages/api/calculate/index.ts
+++ pages/api/calculate/index.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ITriangle, ITriangleResponseData } from '../../../model/Triangle';
 import nc from 'next-connect';
